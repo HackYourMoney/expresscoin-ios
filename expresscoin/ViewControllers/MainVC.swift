003a289c252a9148226d38bd432ac6280ca9a2fa
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-01-09 21:21:08 +0900

    (-)Storyboard (+) Nav,Tabbar

---
+++ expresscoin/ViewControllers/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  MainVC.swift
+//  expresscoin
+//
+//  Created by 이동건 on 2018. 1. 9..
+//  Copyright © 2018년 이동건. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "express-coin"
+    }
+}
