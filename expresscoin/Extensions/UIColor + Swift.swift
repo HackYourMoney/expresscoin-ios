e244f149fdaa46963a306763631b9643e6d4aa46
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-01-11 23:00:00 +0900

    Theme Color

---
+++ expresscoin/Extensions/UIColor + Swift.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+extension UIColor {
+    static var themeDark: UIColor {
+        return UIColor(red: 52.0/255.0, green: 54.0/255.0, blue: 65.0/255.0, alpha: 1.0)
+    }
+}
---
+++ expresscoin/Extensions/UITableView + Swift.swift
@@ -0,0 +1,9 @@
+//
+//  UITableView + Swift.swift
+//  expresscoin
+//
+//  Created by 이동건 on 2018. 1. 11..
+//  Copyright © 2018년 이동건. All rights reserved.
+//
+
+import Foundation
