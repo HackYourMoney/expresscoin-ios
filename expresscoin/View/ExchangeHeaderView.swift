09d741a3aad78fbd7a2ee81a766b8936bf8f7222
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-01-20 00:04:04 +0900

    exchange logo

---
+++ expresscoin/View/ExchangeHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ExchangeHeaderView.swift
+//  expresscoin
+//
+//  Created by 이동건 on 2018. 1. 19..
+//  Copyright © 2018년 이동건. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeHeaderView: UITableViewHeaderFooterView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
