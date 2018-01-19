c8a323dff819de4dc051ecc14c80f5a214806dfa
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-01-19 20:22:06 +0900

    NumberFormatter Class

--- expresscoin/Model/Coin.swift
+++ expresscoin/Model/Coin.swift
@@ -8,12 +8,25 @@
 
 import Foundation
 
+class Formatters {
+    static let price: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.locale = Locale.current
+        formatter.maximumFractionDigits = 0
+        return formatter
+    }()
+}
 class Coin {
     var exchange:String
     var name:String
+    var price: String
+    var amount: String
     
-    init(exchange: String, name:String) {
+    init(exchange: String, name:String, price: String, amount: String) {
         self.exchange = exchange
         self.name = name
+        self.price = price.replacingOccurrences(of: Formatters.price.groupingSeparator, with: "")
+        self.amount = amount.replacingOccurrences(of: Formatters.price.groupingSeparator, with: "")
     }
 }
